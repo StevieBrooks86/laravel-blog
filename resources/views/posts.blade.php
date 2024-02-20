f0e15021c20098d7932bf7f34e396471362b9970
Steven Brooks <steven@expression.digital>
2024-02-20 14:52:59 +0000

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <title>Posts</title>
+</head>
+<body>
+    <h1>Blog posts</h1>
+
+        <article>
+            <a href="/posts/my-first-post"><h2>Post One</h2></a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad error explicabo fugiat sequi vero! Ad
+                asperiores aut culpa dolores eaque facilis fuga ipsum, laudantium maxime neque nihil possimus quo
+                rerum.</p>
+        </article>
+
+    <article>
+        <a href="/posts/my-second-post"><h2>Post Two</h2></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet aperiam asperiores commodi cum, debitis exercitationem id, ipsum iusto mollitia nostrum obcaecati provident ratione recusandae unde! Aliquam enim itaque numquam.</p>
+    </article>
+
+    <article>
+        <a href="posts/my-third-post"><h2>Post Three</h2></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, facere, laboriosam. Eum omnis qui quo repellat sequi. Blanditiis consequatur delectus explicabo illo, omnis quis rem rerum saepe sapiente tempora unde.</p>
+    </article>
+    <a href="/">Go Back</a>
+
+</body>
+</html>
