0f833dd731a0ee55f6e2f7f533612fa1bd87d379
Steven Brooks <steven@expression.digital>
2024-02-23 13:21:50 +0000

    update find functionality

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,6 @@ Route::get('/posts', function () {
 Route::get('posts/{post}', function ($slug) {
 
     return view('post', [
-       'post' => Post::find($slug)
+       'post' => Post::findOrFail($slug)
     ]);
 });
