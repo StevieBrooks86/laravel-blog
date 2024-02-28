403c85b2738dc7e83ee1675f2f2a57a888090b8a
Steven Brooks <steven@expression.digital>
2024-02-28 15:11:55 +0000

    bug fix: can access posts by author now

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -12,7 +12,7 @@
             </a>
 
             <p>
-                By <a href="authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->name }}">{{ $post->category->name }}</a>
+                By <a href="authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             </p>
 
 
